e05c7c405fe0e529b2cc8959e7062e432b2bad67
Gilbert.Gutual <gilbert.gutual@dynata.com>
2023-03-04 00:21:22 +0800

    3/4/2023 12:21am

--- rnd-js-chatbot.js
+++ rnd-js-chatbot.js
@@ -1,14 +1,31 @@
-jQuery.js_botchat = function( settings ) {
+jQuery.chatbot = function( settings ) {
     settings = jQuery.extend(true, {
         debug:true,
+        stage:".stage",
         INT_INPUT_LENGTH:0,
         DONE_TEXT:"Please scroll down and click the ‘Continue’ arrow",
     }, settings || {});
 
     let $ = jQuery;
+    let container = `<div id="container-2" class="w3-card-4 w3-mobile w3-display-bottommiddle w3-round-xlarge">
+        <div class="w3-container w3-animate-bottom js-stage">
+            <div class="w3-container message-container">
+
+            </div>
+            <div class="w3-container w3-border w3-round-xlarge send-container">
+                <input type="text" class="w3-input no-outline message-input" placeholder="Aa">
+                <button class="w3-button fa fa-paper-plane w3-ripple w3-hover-indigo w3-right-align w3-round-xlarge w3-margin-left w3-xlarge message-send"></button>
+            </div>
+        </div>
+        <div class="w3-container next_button w3-padding-16 w3-margin-bottom w3-hide">
+            <div class="w3-button w3-display-bottommiddle w3-margin-bottom w3-indigo w3-round-xlarge next_button">Continue</div>
+        </div>
+    </div>`;
     //onLoad
     onLoad();
+
     function onLoad() {
+        jQuery(settings.stage).append(jQuery(container))
         generateChatDom();
         jQuery(".message-input").on('keyup', function (e) { if (e.key === 'Enter' || e.keyCode === 13) { sendMessage(e); } });
         jQuery(".message-send").on('click', function (e) { sendMessage(); });
@@ -31,6 +48,10 @@ jQuery.js_botchat = function( settings ) {
         let thisquestion = `<div class='message question last w3-panel w3-animate-left w3-padding w3-left w3-round-xlarge init' data-msgindx='${(jQuery(".cm-response-container").length)}'><span class='content'>${settings.DONE_TEXT}</span>
                         <div class='typing-indicator' data-msgindx='${(jQuery(".cm-response-container").length)}'><span class='dot one'></span><span class='dot two'></span><span class='dot three'></span></div></div>`;
         jQuery(".message-container").append(jQuery(thisquestion));
+        //scroll to bottom
+        jQuery('.message-container').animate({
+            scrollTop: 99999999999
+        }, 50);
     }
     function sendMessage(e){
         // Do something
@@ -55,7 +76,7 @@ jQuery.js_botchat = function( settings ) {
         if ((jQuery('.message-container').prop("scrollHeight")-500) > 500) { jQuery('.message-container').addClass("reached")}
         //scroll to bottom
         jQuery('.message-container').animate({
-            scrollTop: 99999
+            scrollTop: 99999999999
         }, 50);
         setTimeout(function() {
             jQuery(`.typing-indicator[data-msgindx='${thiscode}']`).removeClass("activate");
@@ -69,6 +90,11 @@ jQuery.js_botchat = function( settings ) {
             } else {
                 jQuery(".w3-container.next_button").addClass("w3-show")
             }
+
+            //scroll to bottom
+            jQuery('.message-container').animate({
+                scrollTop: 99999999999
+            }, 50);
         }, 1500);
     }
 
